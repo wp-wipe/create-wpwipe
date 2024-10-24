1e5c1c8daacc3e03d3ee6eccbf0ca3b933664f2d
Simon Mongeau <simon@imagerie.ca>
2024-10-24 09:25:53 -0400

    Fix to improve usability at setup

--- templates/theme/index.php
+++ templates/theme/index.php
@@ -4,7 +4,7 @@ get_header();
 while (have_posts()) {
     the_post();
 ?>
-    <article>
+    <article class="is-layout-constrained">
         <h1><?php the_title() ?></h1>
         <?php the_content(); ?>
     </article>
